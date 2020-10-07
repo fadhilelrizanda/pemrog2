7df9cb6acec2dd8a64d90f4f234ddd153974c810
fadhilelrizanda <fadhilelrizanda@gmail.com>
2020-10-07 15:18:35 +0700

    add

---
+++ Fandi/one.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include "one.h"
+
+One::One(int a, int b)
+{
+    x = a;
+    y = b;
+};
+int One::getX()
+{
+    return x;
+}
+int One::getY()
+{
+    return y;
+}
---
+++ Fandi/one.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#ifndef ONE_H
+#define ONE_H
+
+class One
+{
+public:
+    int x, y;
+    One(int a, int b);
+    int getX();
+    int getY();
+};
+
+#endif
